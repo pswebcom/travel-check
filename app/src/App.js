53966acd887706608864d686ff97a0ca7dfa6412
pswebcom <pswebco@gmail.com>
2024-06-30 22:38:25 -0400

    created controlled element  user input

--- app/src/App.js
+++ app/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: true },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -15,20 +17,23 @@ export default function App() {
   );
 }
 
-//FIXME:
-//TODO:
-
 function Logo() {
   return <h1>🌴 Far away</h1>;
 }
 
 function Form() {
+  //todo:**So in React,for FORMS we usually like to keep all this state in just one central place.So inside the React application and not inside the DOM.And so in order to do that we use a technique called controlled elements.And so with this technique it is React who controls and owns the state of these input fields and no longer the DOM.So since we want to now keep this data,inside the application, what that means is that we need some state, right? Because that form data of course changes over time and we also want to maintain our application.
+
+  //todo **so there are 3 steps
+
+  //todo:** step 1-- create state
+  const [description, setDescription] = useState("");
+
   function handleSubmit(e) {
     e.preventDefault();
   }
 
   return (
-    //todo:when clicked onSubmit gets event object which it passes to handleSubmit
     <form className="add-form" onSubmit={(e) => handleSubmit(e)}>
       <h3>What do you need for your trip 😊</h3>
       <select>
@@ -40,7 +45,15 @@ function Form() {
           </option>
         ))}
       </select>
-      <input type="text" placeholder="item..." />
+      //todo:*** step 2--use state as a value of input ie value={description}
+      //todo:*** step 3--connect setDescription state func with user onChange
+      event
+      <input
+        type="text"
+        placeholder="item..."
+        value={description}
+        onChange={(e) => setDescription}
+      />
       <button>Add</button>
     </form>
   );
